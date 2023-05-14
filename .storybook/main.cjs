75cc739e678838a20e92267392f5b0aa27399c32
Sivaprakasam <66865924+SivaPrakasam7@users.noreply.github.com>
2023-05-14 06:41:36 +0000

    migration

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,5 +1,3 @@
-const path = require("path");
-
 module.exports = {
   stories: [
     "../stories/**/*.stories.mdx",
@@ -10,10 +8,23 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@bbbtech/storybook-formik/register",
+    "@storybook/addon-mdx-gfm",
   ],
-  framework: "@storybook/react",
-  features: {
-    storyStoreV7: true,
-    previewMdx2: true,
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: { fastRefresh: true },
+  },
+  docs: {
+    autodocs: true,
   },
 };
